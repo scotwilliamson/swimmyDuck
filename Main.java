62b85e238b87feb749a204874ea9ffeec0ebd964
Andrew Howard <andrewhoward@crescentschool.org>
2017-05-01 15:22:20 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,47 @@
 package application;
 	
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Pane gameLayout = new Pane();
+			Scene scene = new Scene(gameLayout,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			primaryStage.setTitle("Swimmy Duck");
+						
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		new AnimationTimer()
+		{
+			public void handle(long currentNanoTime)
+			{
+				update();
+			}
+		}.start();
+	}
+	
+	private static class Player extends GameObject {
+		Player(){
+			super(new Circle(15, 15, 15, Color.ORANGE));
+		}
+	}
+	
+	public static void update(){
+		
 	}
 	
 	public static void main(String[] args) {
